19bba7b21df248a4171a408d066256e3295e5ebc
Leo Basov <l.basov@sparc-industries.com>
2024-12-31 12:50:37 +0100

    added base initialization functions

--- src/Pot8o.jl
+++ src/Pot8o.jl
@@ -1,5 +1,34 @@
 module Pot8o
 
-greet() = print("Hello World!")
+export po_initialize
+export po_finalize
+
+using MPI
+using T8code
+using T8code.Libt8: sc_init
+using T8code.Libt8: sc_free
+using T8code.Libt8: sc_finalize
+using T8code.Libt8: sc_array_new_data
+using T8code.Libt8: sc_array_destroy
+using T8code.Libt8: SC_LP_ESSENTIAL
+using T8code.Libt8: SC_LP_PRODUCTION
+
+function po_initialize()
+    # Initialize MPI. This has to happen before we initialize sc or t8code.
+    mpiret = MPI.Init()
+
+    # We will use MPI_COMM_WORLD as a communicator.
+    comm = MPI.COMM_WORLD
+
+    # Initialize the sc library, has to happen before we initialize t8code.
+    sc_init(comm, 1, 1, C_NULL, SC_LP_ESSENTIAL)
+    # Initialize t8code with log level SC_LP_PRODUCTION. See sc.h for more info on the log levels.
+    t8_init(SC_LP_PRODUCTION)
+end
+
+function po_finalize()
+    t8_global_productionf("finalizing\n")
+    sc_finalize()
+end
 
 end # module Pot8o
